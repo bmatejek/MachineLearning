da94e127e14c57fe781c8e99a4cc7db9944c0e23
bmatejek525 <bmatejek@gmail.com>
2015-05-15 15:45:54 -0400

    Merging Mushroom and Learner

--- Mushroom.java
+++ Mushroom.java
@@ -11,7 +11,7 @@
 /*****************************************************************************/
 
 
-
+import MachineLearning.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
